07995d2d78181d040ff4b30dbbe95fed7e150ad8
younes313 <younesmoradi313@gmail.com>
2020-08-07 18:19:53 +0430

    fix: flake8ize the code

--- app/core/tests/test_model.py
+++ app/core/tests/test_model.py
@@ -8,7 +8,10 @@ class ModelTest(TestCase):
         """test that create a new user woth email is succefully done"""
         email = "test@test.com"
         password = "test123"
-        user = get_user_model().objects.create_user(email=email, password=password)
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
@@ -16,14 +19,20 @@ class ModelTest(TestCase):
     def test_email_is_normalized(self):
         """test that new user email is normalized"""
         email = "test@TESt.cOM"
-        user = get_user_model().objects.create_user(email=email, password="test123")
+        user = get_user_model().objects.create_user(
+            email=email,
+            password="test123"
+        )
 
         self.assertEqual(user.email, email.lower())
 
     def test_new_user_should_have_email(self):
         """test that new user requires email"""
         with self.assertRaises(ValueError):
-            get_user_model().objects.create_user(email=None, password="test123")
+            get_user_model().objects.create_user(
+                email=None,
+                password="test123"
+            )
 
     def test_create_superuser(self):
         """test that createsuper user works"""
